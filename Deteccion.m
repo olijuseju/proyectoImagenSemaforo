760130e886e500ff9dbefdb0703d06d857bef301
Jose Julio <jjpeajar@epsg.upv.es>
2022-01-25 19:21:55 +0100

    Version 2

--- Deteccion.m
+++ Deteccion.m
@@ -1,31 +1,38 @@
-function output = Deteccion(imagenBase)
-
-
-    Img = imagenBase; 
+function output = Deteccion(imgOrigen)
+    Img = imgOrigen; 
     Img = rgb2gray(Img); % Pasar a escala de grises
-    
-    
+   
     vehicleDetector = vehicleDetectorACF('front-rear-view'); % Detector de vehiculos
     detectorPeople = peopleDetectorACF('inria-100x41'); % Detector de personas
     
+    numCoches = 0;
+    numPersonas = 0;
+    
     % Deteccion vehiculos
     [cboxes,cscores] = detect(vehicleDetector,Img);
-    indice = find(cscores > 4);
-    pV = cscores(indice);
-    cV = cboxes(indice,:);
-    Img = insertObjectAnnotation(Img,'rectangle',cV,pV);
+    if not (isempty(cboxes))
+        indice = find(cscores > 4);
+        puntuacioncochesBuenos = cscores(indice);
+        cochesBuenos = cboxes(indice,:);
+        Img = insertObjectAnnotation(Img,'rectangle',cochesBuenos,puntuacioncochesBuenos);
+        numCoches = size(cochesBuenos);
+    end
     
     
     % Deteccion personas
     [pboxes,pscores] = detect(detectorPeople,Img);
-    if not (isempty(cboxes))
-        Img = insertObjectAnnotation(Img,'rectangle',pboxes,pscores);
+    if not (isempty(pboxes))
+        indice2 = find(pscores > 4);
+        puntuacionpeatonesBuenos = pscores(indice2);
+        peatonesBuenos = pboxes(indice2,:);
+        Img = insertObjectAnnotation(Img,'rectangle',peatonesBuenos,puntuacionpeatonesBuenos);
+        numPersonas = size(peatonesBuenos);
+
     end
-        
-    numCoches = size(cboxes);
-    numPersonas = size(pboxes);
     
-    % Outputs
+    
+    
+    % Return
     output = [numCoches(1); numPersonas(1)];
 end
 
